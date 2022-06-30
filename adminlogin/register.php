77e262f0154e709c003c00d34746c2e04d55792e
Varun Rai <77796938+kgrai@users.noreply.github.com>
2022-06-30 17:32:36 +0530

    Add files via upload

---
+++ adminlogin/register.php
@@ -0,0 +1,66 @@
+<?php include('server.php') ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="stylesone.css">
+	<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <title>Registration</title>
+</head>
+<body>
+  
+<div class="container" id="container">
+	<div class="form-container sign-up-container">
+		<form action="#">
+			<h1>Create Account</h1>
+			<div class="social-container">
+				<a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
+				<a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
+				<a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
+			</div>
+			<span>or use your email for registration</span>
+			<input type="text" placeholder="Name" />
+			<input type="email" placeholder="Email" />
+			<input type="password" placeholder="Password" />
+			<button>Sign Up</button>
+		</form>
+	</div>
+	<div class="form-container sign-in-container">
+		<form method="post" action="register.php">
+			<?php include('errors.php'); ?>
+			<h1>Register</h1>
+			<a href="#"></a>
+			<input type="username" name="username" placeholder="usn" value="<?php echo $username; ?>" />
+			<input type="email" name="email" placeholder="Email" value="<?php echo $email; ?>" />
+			<input type="password" name="password_1" placeholder="Password" />
+			<input type="password" name="password_2" placeholder="confirm Password" />
+			<a href="#"></a>
+			<button type="submit" name="reg_user">Sign Up</button>
+			<p><a href="https://knowyourcgpa.tech">continue as guest</a></p>
+		</form>
+	
+	</div>
+	<div class="overlay-container">
+		<div class="overlay">
+			<div class="overlay-panel overlay-left">
+				<h1>Welcome Back!</h1>
+				<p>To keep connected with us please login with your personal info</p>
+				<button class="ghost" id="signIn">Sign In</button>
+			</div>
+			<div class="overlay-panel overlay-right">
+				<h1>Hello, Friend!</h1>
+				<p>Enter your personal details and start journey with us</p>
+				<p>
+					Already a member? <a href="login.php">Sign in</a>
+				</p>
+				
+			</div>
+		</div>
+	</div>
+</div>
+
+    
+</body>
+</html>
